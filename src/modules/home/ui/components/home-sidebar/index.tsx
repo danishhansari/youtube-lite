e64cf95be591caf15139546f1fbbb68b718aeced
Danish <dan71ish@gmail.com>
2025-04-20 22:56:14 +0530

    added the sidebar and worked on navbar

---
+++ src/modules/home/ui/components/home-sidebar/index.tsx
@@ -0,0 +1,16 @@
+import { Sidebar, SidebarContent } from "@/components/ui/sidebar";
+import { MainSection } from "./main-section";
+import { Separator } from "@/components/ui/separator";
+import { PersonalSection } from "./person-section";
+
+export const HomeSidebar = () => {
+  return (
+    <Sidebar className="pt-16 z-40 border-none" collapsible="icon">
+      <SidebarContent className="bg-background">
+        <MainSection />
+        <Separator className="w-[92%] mx-auto" />
+        <PersonalSection />
+      </SidebarContent>
+    </Sidebar>
+  );
+};
---
+++ src/modules/home/ui/components/home-sidebar/person-section.tsx
@@ -0,0 +1,59 @@
+"use client";
+
+import {
+  SidebarGroup,
+  SidebarGroupContent,
+  SidebarGroupLabel,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+} from "@/components/ui/sidebar";
+import { History, ListVideoIcon, ThumbsUpIcon } from "lucide-react";
+import Link from "next/link";
+
+const items = [
+  {
+    title: "History",
+    url: "/playlists/history",
+    icon: History,
+    auth: true,
+  },
+  {
+    title: "Liked video",
+    url: "/playlists/liked",
+    icon: ThumbsUpIcon,
+    auth: true,
+  },
+  {
+    title: "All Playlists",
+    url: "/playlists",
+    icon: ListVideoIcon,
+  },
+];
+
+export const PersonalSection = () => {
+  return (
+    <SidebarGroup>
+      <SidebarGroupLabel>You</SidebarGroupLabel>
+      <SidebarGroupContent>
+        <SidebarMenu>
+          {items.map((item) => (
+            <SidebarMenuItem key={item.title}>
+              <SidebarMenuButton
+                tooltip={item.title}
+                asChild
+                isActive={false} // TODO: Change to look at current path name
+                onClick={() => {}} // TODO: Do something
+              >
+                <Link href={item.url}>
+                  <item.icon />
+                  <span className="text-sm">{item.title}</span>
+                </Link>
+              </SidebarMenuButton>
+            </SidebarMenuItem>
+          ))}
+        </SidebarMenu>
+      </SidebarGroupContent>
+    </SidebarGroup>
+  );
+};
